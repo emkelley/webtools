f0ec216caa5dd03cde647dce7c5b44a724b75030
Eric Kelley <hello@ericmkelley.com>
2023-07-03 17:59:57 -0400

    add fathom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    head: {
+      script: [
+        {
+          src: "https://cdn.usefathom.com/script.js",
+          type: "text/javascript",
+          defer: true,
+          "data-site": "VYFIBJEV",
+        },
+      ],
+    },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/google-fonts",
